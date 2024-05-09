6189aa5dfa8cde1b0591ce3f59e0933a34d45f7c
tal1b7 <tatyene.alves@scania.com>
2024-05-09 16:59:24 -0300

    feat: modal delete

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,8 +2,25 @@ import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Product } from '../../shared/interfaces/product';
 import { ProductsService } from '../../shared/service/products.service';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  template: `
+  <h2 mat-dialog-title>Delete file</h2>
+  <mat-dialog-content>
+    Would you like to delete cat.jpeg?
+  </mat-dialog-content>
+  <mat-dialog-actions>
+    <button mat-button mat-dialog-close>No</button>
+    <button mat-button mat-dialog-close cdkFocusInitial>Ok</button>
+  </mat-dialog-actions>`,
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
+})
+export class ConfirmationDialogComponent {}
 
 @Component({
   selector: 'app-list',
@@ -17,7 +34,7 @@ export class ListComponent {
   products: Product[] = [];
   productService = inject(ProductsService);
   router = inject(Router);
-
+  matDialog = inject(MatDialog);
   ngOnInit() {
     this.productService.getAll().subscribe((products) => {
       this.products = products;
@@ -26,5 +43,13 @@ export class ListComponent {
 
   onEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
+  }
+
+  onDelete(product: Product) {
+    this.matDialog.open(ConfirmationDialogComponent)
+    .afterClosed()
+    .subscribe((data) => {
+      console.log('afterClosed', data);
+    });
   }
 }
